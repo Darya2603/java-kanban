0805a7313ba7d9863706deb0783069d878744c04
PiTri <burova.dv@yandex.ru>
2024-12-30 12:56:19 +0300

    Merge remote-tracking branch 'origin/sprint_7-solution-in-file-manager' into sprint_7-solution-in-file-manager

--- src/tracker/manager/impl/InMemoryTaskManagerImpl.java
+++ src/tracker/manager/impl/InMemoryTaskManagerImpl.java
@@ -79,7 +79,7 @@ public class InMemoryTaskManagerImpl implements TaskManager {
                 .filter(existingTask -> isOverlapping(newTask, existingTask))
                 .findFirst()
                 .ifPresentOrElse(
-                        _ -> {
+                        overlappedTask -> {
                             if (newTask instanceof Subtask) {
                                 throw new IllegalArgumentException("Подзадача пересекается с существующей подзадачей.");
                             } else {
